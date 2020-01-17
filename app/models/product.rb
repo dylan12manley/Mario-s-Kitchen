5643db7d6500d1de39a1f09c8e482adc4a95755b
Dylan Manley <dylan.manley@gmail.com>
2020-01-17 11:37:19 -0800

    create titleize  method for prject.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  before_save(:titleize_name)
+
+  private
+    def titleize_name
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it("titleizes the name of an product") do
+    product = Product.create({name: "cripps Pink lady", cost: 1.50, country_of_origin: "Canada"})
+    expect(product.name()).to(eq("Cripps Pink Lady"))
+  end
   it ( "creates a new product") do
     product = Product.create({name: "Apple", cost: 1.10, country_of_origin: "USA" })
     expect(product.name).to(eq("Apple"))
