b59e56c9fd50099e551ea94279afb5c015680d12
Dylan Manley <dylan.manley@gmail.com>
2020-01-17 10:55:55 -0800

    create CRUN methods for Products class

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
 
-def create
+  def index
+    @products = Product.all
+    @reviews = Reviews.all
+    render :index
+  end
+
+  def new
+    @product = Division.new
+    render :new
+  end
+
+  def create
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = "Product successfully created."
@@ -9,3 +20,35 @@ def create
       render :new
     end
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name)
+  end
+end
