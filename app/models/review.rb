8df2b442f0b26c2e0c667f1e17c262a7eeee750e
Dylan Manley <dylan.manley@gmail.com>
2020-01-17 11:05:56 -0800

    create a validaton for content_body length

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates_length_of :content_body, in: 50..250,
+  message: "review must be longer than 50 charactors and shorter than 250."
 end
