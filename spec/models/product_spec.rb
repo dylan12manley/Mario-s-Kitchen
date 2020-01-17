bbbfd4c1d8d813e379dae8163a78b6b96c605cc1
Dylan Manley <dylan.manley@gmail.com>
2020-01-17 10:40:21 -0800

    make a test for create method for products class

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it ( "creates a new product") do
+    product = Product.create({name: "Apple", cost: 1.10, country_of_origin: "USA" })
+      expect(product).to(eq(name: "Apple", cost: 1.10, country_of_origin: "USA"))
+  end
 end
